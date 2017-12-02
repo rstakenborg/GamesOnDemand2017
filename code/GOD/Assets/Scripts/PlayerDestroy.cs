5dba0d8b738015dabff34fd32a3ae42cb1c0766b
asmallshellscript <rene.stakenborg@gmail.com>
2017-12-02 10:02:06 -0500

    * CHECKIN before auto API upgrade for mesh render

---
+++ code/GOD/Assets/Scripts/PlayerDestroy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDestroy : MonoBehaviour {
+
+    void Start()
+    {
+        Application.runInBackground = true;
+    }
+
+    void Update()
+    {
+        FloorPadInput.GetEvents(gameObject);
+    }
+
+    void OnTilePressed(Vector2 coords)
+    {
+        //Debug.Log("Entered " + coords);
+        //CreateNewBall(new Vector3(coords.x, 10, -coords.y));
+    }
+
+    void OnTileReleased(Vector2 coords)
+    {
+        Destroy(this.gameObject);
+        //Debug.Log("Leaving " + coords);
+        //CreateNewBall (new Vector3 (coords.x, 10, coords.y));
+    }
+
+}
---
+++ code/GOD/Assets/Scripts/TunnelMesh.cs
@@ -0,0 +1,21 @@
+﻿//http://wiki.unity3d.com/index.php/MeshHelper
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TunnelMesh : MonoBehaviour {
+
+    public MeshFilter MF;
+
+    void Start()
+    {
+        Mesh mesh = MF.mesh;
+        MeshHelper.Subdivide(mesh, 6);   // divides a single quad into 6x6 quads
+        MF.mesh = mesh;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
