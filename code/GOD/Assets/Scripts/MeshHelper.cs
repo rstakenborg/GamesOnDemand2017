5e6753895ddc3c4761ee5a19434d62c397b8f9e8
asmallshellscript <rene.stakenborg@gmail.com>
2017-12-02 10:54:37 -0500

    * FIXED uv1 conflict and added Editor helper for a PyramidGenerator

--- code/GOD/Assets/Scripts/MeshHelper.cs
+++ code/GOD/Assets/Scripts/MeshHelper.cs
@@ -9,7 +9,6 @@ public static class MeshHelper
     static List<Vector3> normals;
     static List<Color> colors;
     static List<Vector2> uv;
-    static List<Vector2> uv1;
     static List<Vector2> uv2;
 
     static List<int> indices;
@@ -21,7 +20,6 @@ public static class MeshHelper
         normals = new List<Vector3>(mesh.normals);
         colors = new List<Color>(mesh.colors);
         uv = new List<Vector2>(mesh.uv);
-        uv1 = new List<Vector2>(mesh.uv1);
         uv2 = new List<Vector2>(mesh.uv2);
         indices = new List<int>();
     }
@@ -31,7 +29,6 @@ public static class MeshHelper
         normals = null;
         colors = null;
         uv = null;
-        uv1 = null;
         uv2 = null;
         indices = null;
     }
@@ -56,8 +53,6 @@ public static class MeshHelper
             colors.Add((colors[i1] + colors[i2]) * 0.5f);
         if (uv.Count > 0)
             uv.Add((uv[i1] + uv[i2]) * 0.5f);
-        if (uv1.Count > 0)
-            uv1.Add((uv1[i1] + uv1[i2]) * 0.5f);
         if (uv2.Count > 0)
             uv2.Add((uv2[i1] + uv2[i2]) * 0.5f);
 
@@ -97,8 +92,6 @@ public static class MeshHelper
             mesh.colors = colors.ToArray();
         if (uv.Count > 0)
             mesh.uv = uv.ToArray();
-        if (uv1.Count > 0)
-            mesh.uv1 = uv1.ToArray();
         if (uv2.Count > 0)
             mesh.uv2 = uv2.ToArray();
 
@@ -130,8 +123,6 @@ public static class MeshHelper
             colors.Add((colors[i1] + colors[i2] + colors[i3]) / 3.0f);
         if (uv.Count > 0)
             uv.Add((uv[i1] + uv[i2] + uv[i3]) / 3.0f);
-        if (uv1.Count > 0)
-            uv1.Add((uv1[i1] + uv1[i2] + uv1[i3]) / 3.0f);
         if (uv2.Count > 0)
             uv2.Add((uv2[i1] + uv2[i2] + uv2[i3]) / 3.0f);
         return newIndex;
@@ -182,8 +173,6 @@ public static class MeshHelper
             mesh.colors = colors.ToArray();
         if (uv.Count > 0)
             mesh.uv = uv.ToArray();
-        if (uv1.Count > 0)
-            mesh.uv1 = uv1.ToArray();
         if (uv2.Count > 0)
             mesh.uv2 = uv2.ToArray();
 
--- code/GOD/Assets/Scripts/TunnelMesh.cs
+++ code/GOD/Assets/Scripts/TunnelMesh.cs
@@ -1,21 +1,41 @@
 ﻿//http://wiki.unity3d.com/index.php/MeshHelper
+//https://forum.unity.com/threads/perlin-noise-on-mesh-vertices.242200/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TunnelMesh : MonoBehaviour {
 
-    public MeshFilter MF;
-
+    private MeshFilter MF;
+    private Vector3[] vertPositions;
+    private int scaleX = 2;
+    private int scaleY = 2;
+    private int invertscaleX = -2;
+    private int invertscaleY = -2;
     void Start()
     {
+        MF = GetComponent<MeshFilter>();
         Mesh mesh = MF.mesh;
         MeshHelper.Subdivide(mesh, 6);   // divides a single quad into 6x6 quads
         MF.mesh = mesh;
+        vertPositions = mesh.vertices;
+
+        mesh.vertices = AddNoise(vertPositions);
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
     }
 
+    Vector3[] AddNoise(Vector3[] vertices) {
+        // .. todo:: add noise only to the farthest ring?
+        for (var i = 0; i < vertices.Length; i++)
+        {
+            vertices[i].x = vertPositions[i].x + (Mathf.PerlinNoise(Time.deltaTime + (vertPositions[i].x * scaleX), Time.deltaTime + (vertPositions[i].y * scaleY))) - (Mathf.PerlinNoise(Time.deltaTime + (vertPositions[i].x * invertscaleX), Time.deltaTime + (vertPositions[i].y * invertscaleY)));
+        }
+        return vertices;
+    }
     // Update is called once per frame
     void Update () {
-		
-	}
+        Mesh mesh = MF.mesh;
+        mesh.vertices = AddNoise(mesh.vertices);
+    }
 }
