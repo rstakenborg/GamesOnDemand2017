d186181e401da24104bbc4a8fea4bd149b46afae
asmallshellscript <rene.stakenborg@gmail.com>
2017-12-03 16:46:15 -0500

    * FIXED build error which didn't show up locally for some reason

--- code/GOD/Assets/Scripts/SpawnGameObject.cs
+++ code/GOD/Assets/Scripts/SpawnGameObject.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class SpawnGameObject : MonoBehaviour {
     public List<string> prefabNames = new List<string> { "Bacteria" };
