3d47721c0f2d3aa1d58784a6f702d4f51e993061
asmallshellscript <rene.stakenborg@gmail.com>
2017-12-03 15:59:56 -0500

    * FIXED sprite landingstrip

---
+++ code/GOD/Assets/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        // on collision we should 
+        if (col.gameObject.tag == "Baddie") {
+            Destroy(col.gameObject);
+        }
+        Destroy(gameObject);
+    }
+}
