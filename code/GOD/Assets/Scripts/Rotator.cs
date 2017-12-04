924579f0aacde1bf410c8ec78e7e8a3d4be7ea20
jkbh-86 <jon.hinchley.developer@gmail.com>
2017-12-03 23:54:47 -0500

    Environmental Test
    Testing a trick of displaying the background blood vessel wall in the scene

---
+++ code/GOD/Assets/Scripts/Rotator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	public float RotateX,
+				RotateY,
+				RotateZ;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	/// <summary>
+	/// LateUpdate is called every frame, if the Behaviour is enabled.
+	/// It is called after all Update functions have been called.
+	/// </summary>
+	void LateUpdate()
+	{
+		transform.Rotate(new Vector3(RotateX, RotateY, RotateZ) * Time.deltaTime);
+	}
+}
